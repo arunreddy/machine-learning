a0c67323c7f958309257657f1534fa990ad1dfdd
Arun Reddy <arunreddy.nelakurthi@gmail.com>
2015-07-14 15:15:42 -0700

    Added Math utils subproject. Added degree matrix calculation and laplacian normalization  methods.

---
+++ ml-math-utils/src/main/java/ml/arunreddy/research/mathutils/MathUtilsException.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2015 Arun Reddy Nelakurthi
+* 
+* This file is part of ml-math-utils.
+*
+* ml-math-utils is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* ml-math-utils is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ml.arunreddy.research.mathutils;
+
+/**
+ * MathUtils exception wrapper.
+ * 
+ * @version $Id$
+ */
+public class MathUtilsException extends Exception
+{
+
+    /**
+     * 
+     */
+    public MathUtilsException()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     */
+    public MathUtilsException(String message)
+    {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param cause
+     */
+    public MathUtilsException(Throwable cause)
+    {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     * @param cause
+     */
+    public MathUtilsException(String message, Throwable cause)
+    {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     * @param cause
+     * @param enableSuppression
+     * @param writableStackTrace
+     */
+    public MathUtilsException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+    {
+        super(message, cause, enableSuppression, writableStackTrace);
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ ml-math-utils/src/main/java/ml/arunreddy/research/mathutils/MatrixUtils.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2015 Arun Reddy Nelakurthi
+* 
+* This file is part of ml-math-utils.
+*
+* ml-math-utils is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* ml-math-utils is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ml.arunreddy.research.mathutils;
+
+import org.la4j.Matrix;
+import org.la4j.Vector;
+import org.la4j.matrix.sparse.CCSMatrix;
+
+/**
+ * 
+ * @version $Id$
+ * 
+ */
+public class MatrixUtils
+{
+
+    /**
+     * Returns the degree matrix (row sum of given matrices returned as a diagonal matrix). Used for Laplacian Normalization.
+     * @param matrixArray
+     * @return
+     * @throws MathUtilsException
+     */
+    public Matrix calculateDegreeMatrix(Matrix... matrixArray) throws MathUtilsException{
+
+        if(matrixArray.length < 1){
+            throw new MathUtilsException("Array is empty..!!");
+        }
+        int degreeMatrixRowCount = matrixArray[0].rows();
+        Matrix degreeMatrix = new CCSMatrix(degreeMatrixRowCount,degreeMatrixRowCount);
+        for (int i = 0; i < matrixArray.length; i++) {
+            Matrix matrix = matrixArray[i];
+            //Check if the number of rows of the matrix equals degree matrix row count.
+            if(degreeMatrixRowCount!=matrix.rows()){
+                throw new MathUtilsException("Rows of the degree matrix doesn't match with matrix-#("+i+") in the given argument.");
+            }
+            for (int j = 0; j < degreeMatrixRowCount; j++) {
+                Vector rowVector = matrix.getRow(j);
+                double currentValue = degreeMatrix.get(j,j);
+                degreeMatrix.set(j, j, rowVector.sum()+currentValue);
+            }
+        }
+        
+        return degreeMatrix;
+    }
+    
+    /**
+     * Calculate Laplacian Normal Matrix.
+     * 
+     * @param degreeMatrixA
+     * @param adjacencyMatrix
+     * @param degreeMatrixB
+     * @return
+     */
+    public Matrix calculateLaplacianNorm(Matrix degreeMatrixA, Matrix adjacencyMatrix, Matrix degreeMatrixB){
+        return degreeMatrixA.multiply(adjacencyMatrix).multiply(degreeMatrixB);
+    }
+
+}
